9ae6cdd42c0b8356863ac70ae6675837db22a3fa
Joseph David <techiejossy@gmail.com>
2021-07-01 19:39:23 +0100

    feat: Widen authorisation scope

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -3,14 +3,17 @@ import NextAuth from 'next-auth'
 import Providers from 'next-auth/providers'
 
 dotenv.config()
+const scopes = "user-read-recently-played user-top-read user-read-playback-position user-read-playback-state user-modify-playback-state user-read-currently-playing app-remote-control streaming playlist-modify-public playlist-modify-private playlist-read-private playlist-read-collaborative user-follow-modify user-follow-read user-library-modify user-library-read user-read-email user-read-private"
 
 export default NextAuth({
+
 
   providers: [
 
     Providers.Spotify({
       clientId: process.env.SPOTIFY_CLIENT_ID,
-      clientSecret: process.env.SPOTIFY_CLIENT_SECRET
+      clientSecret: process.env.SPOTIFY_CLIENT_SECRET,
+      scope: scopes
     })
 
   ],
