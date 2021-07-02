b8ab99758785c289a59305b2e29f13a3ed7c7727
Joseph David <techiejossy@gmail.com>
2021-07-02 15:15:55 +0100

    Customise Header content

--- components/Header.js
+++ components/Header.js
@@ -15,11 +15,21 @@ const Header = () => {
 
         <Header style={{ display: "flex", justifyContent: "space-between" }}>
             <Link href="/">
-            <div className={styles.logo}>OctoPlay</div>
+                <div className={styles.logo}>OctoPlay</div>
             </Link>
             {/* <img src={`https://www.countryflags.io/${data.country}/shiny/64.png`} /> */}
             <Menu theme="dark" mode="horizontal">
-                <Menu.Item key={session.user.id}>
+                <Menu.Item key='1'>
+                    <Link href='/ta'>
+                        Top Artists
+                    </Link>
+                </Menu.Item>
+                <Menu.Item key='2'>
+                    <Link href='/tt'>
+                        Top Tracks
+                    </Link>
+                </Menu.Item>
+                <Menu.Item key='3'>
                     <Avatar src={session.user.picture} style={{ marginRight: "0.5rem" }} draggable="false" alt="User's profile picture" />
                     {session.user.name}
                 </Menu.Item>
@@ -35,5 +45,5 @@ export default Header
 export async function getServerSideProps(ctx) {
 
     const session = await getSession(ctx)
-  
-  }
+
+}
