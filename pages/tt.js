89e0a9de2d681b09e59547157609d2009581b9a5
Joseph David <techiejossy@gmail.com>
2021-07-02 15:17:13 +0100

    feat: Add 'Top Tracks' page

--- pages/tt.js
+++ pages/tt.js
@@ -5,34 +5,37 @@ import Header from '../components/Header'
 import Footer from '../components/Footer'
 import { Meta } from 'antd/lib/list/Item'
 
-export default function topArtists({ data }) {
+export default function topTracks({ data }) {
 
-    const [session, loading] = useSession()
     const { Content } = Layout
+    const favTracks = data.items
+    const favTracksClean = favTracks.map(track => {
+        console.log(track.name, track.album.name)
+    })
+    { console.log(favTracks) }
 
     return <>
 
         <Layout style={{ height: '100vh' }}>
-            <Header />
 
-            {console.log(data)}
+            <Header />
             <Content style={{ display: "flex", flexDirection: "column", alignItems: 'center', height: "100%", justifyContent: "center", overflow: "scroll" }}>
 
-                <h1>Your Top Artists</h1>
+                <h1>Top Tracks</h1>
 
                 <Row gutter={50}>
 
-                    {data.items.map(artist => (
+                    {data.items.map(track => (
 
                         <Col>
                             <Card
-                                key={artist.id}
-                                cover={<img alt={`${artist.name}'s picture`} src={artist.images[0].url} style={{ height: 300, width: 300 }} />}
+                                key={track.id}
+                                cover={<img alt={`${track.name}'s picture`} src={track.album.images[0].url} style={{ height: 300, width: 300 }} />}
                                 style={{ width: 300 }}
-                                actions={[<p>Genre: <span style={{ fontWeight: 'bold' }}>{artist.genres[0]}</span></p>]}>
+                                actions={[<p style={{ fontWeight: 'bold' }}><a href={track.album.external_urls.spotify} target="_blank">{track.album.name}</a></p>]}>
 
                                 <Meta
-                                    title={<a href={artist.external_urls.spotify} target="_blank">{artist.name}</a>} style={{ textAlign: 'center' }} />
+                                    title={<a href={track.external_urls.spotify} target="_blank">{track.name}</a>} style={{ textAlign: 'center' }} />
                             </Card>
                         </Col>
 
@@ -41,8 +44,8 @@ export default function topArtists({ data }) {
                 </Row>
 
             </Content>
-
             <Footer />
+
         </Layout>
 
     </>
@@ -52,7 +55,7 @@ export async function getServerSideProps(ctx) {
 
     const session = await getSession(ctx)
 
-    const type = 'artists'
+    const type = 'tracks'
     const limit = 5
     const range = 'short_term'
     const res = await fetch(`https://api.spotify.com/v1/me/top/${type}?time_range=${range}&limit=${limit}`, {
