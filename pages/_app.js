88d813c63e5ebdb7ac531f43f38c3bba14e5b47c
Joseph David <techiejossy@gmail.com>
2021-07-07 00:05:14 +0100

    feat: Display progress bar while fetching external data using NProgress

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,19 @@
+import NProgress from 'nprogress'
+import "nprogress/nprogress.css";
+import Router from 'next/dist/next-server/lib/router/router';
 import { Provider } from 'next-auth/client'
 import '../styles/globals.css'
+
+NProgress.configure({
+  minimum: 0.3,
+  easing: 'ease',
+  speed: 500,
+  showSpinner: false,
+});
+
+Router.events.on('routeChangeStart', () => NProgress.start());
+Router.events.on('routeChangeComplete', () => NProgress.done());
+Router.events.on('routeChangeError', () => NProgress.done());
 
 function MyApp({ Component, pageProps }) {
 
