a81da2a4b2ce98f4343298b58c710f4c6efd7855
Joseph David <techiejossy@gmail.com>
2021-07-18 20:05:48 +0100

    fix: remove 'dotenv' dependency and usage

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,12 +1,10 @@
-import dotenv from 'dotenv'
 import NextAuth from 'next-auth'
 import Providers from 'next-auth/providers'
 
-dotenv.config()
+// Spotify User scopes
 const scopes = "user-read-recently-played user-top-read user-read-playback-position user-read-playback-state user-modify-playback-state user-read-currently-playing app-remote-control streaming playlist-modify-public playlist-modify-private playlist-read-private playlist-read-collaborative user-follow-modify user-follow-read user-library-modify user-library-read user-read-email user-read-private"
 
 export default NextAuth({
-
 
   providers: [
 
