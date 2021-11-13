e004321ca6da692b2888b3a0fad670d23300099b
Joseph David <techiejossy@gmail.com>
2021-11-13 07:57:15 +0100

    update: Strip components of Ant Design elements

--- components/Footer.js
+++ components/Footer.js
@@ -1,18 +1,15 @@
 import React from 'react'
-import { Layout, Button, Menu, Avatar } from 'antd'
 
 const Footer = () => {
-
-    // Layout components
-    const { Footer } = Layout
 
     return <>
 
         <Footer>
-            <p style={{ textAlign: "center" }}>Built with NextJS and Ant Design</p>
+            <p style={{ textAlign: "center" }}>Built with NextJS</p>
         </Footer>
 
     </>
+
 }
 
 export default Footer
--- components/Header.js
+++ components/Header.js
@@ -1,51 +1,19 @@
 import React from 'react'
-import { Layout, Button, Menu, Avatar } from 'antd'
 import Link from 'next/link'
 import { useSession } from 'next-auth/client'
-import 'antd/dist/antd.css';
-import styles from '../styles/Profile.module.css'
 
 const Header = () => {
 
     const [session, loading] = useSession()
-    // Layout components
-    const { Header } = Layout
+
 
     return <>
 
-        <Header style={{ display: "flex", justifyContent: "space-between" }}>
-            <Link href="/">
-                <div className={styles.logo} style={{ cursor: 'pointer' }}>OctoPlay</div>
-            </Link>
-            
-            <Menu theme="dark" mode="horizontal">
-                <Menu.Item key='1'>
-                    <Link href='/ta'>
-                        Top Artists
-                    </Link>
-                </Menu.Item>
-                <Menu.Item key='2'>
-                    <Link href='/tt'>
-                        Top Tracks
-                    </Link>
-                </Menu.Item>
-                <Menu.Item key='3'>
-                    <Link href='/yp'>
-                        Recent Playlists
-                    </Link>
-                </Menu.Item>
-                <Menu.Item key='4'>
-                    <Link href='/fa'>
-                        Artists You Follow
-                    </Link>
-                </Menu.Item>
-                <Menu.Item key='5'>
-                    <Link href='/profile'>
-                        <Avatar src={session.user.picture} style={{ marginRight: "0.5rem" }} draggable="false" alt="User's profile picture" />
-                    </Link>
-                </Menu.Item>
-            </Menu>
-        </Header>
+        <nav>
+            <ul>
+                <li>Home</li>
+            </ul>
+        </nav>
 
     </>
 }
