922c3daef8cb08f2e7a2dcef62957fc580a38afc
Joseph David <techiejossy@gmail.com>
2022-08-01 07:02:01 +0100

    feat: Refresh token rotation

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,9 +1,50 @@
+import moment from "moment";
 import NextAuth from "next-auth";
 import SpotifyProvider from "next-auth/providers/spotify";
+import { convertTime } from "../../../utils/utils";
 
 // Spotify permission scopes
 const scopes =
   "user-read-recently-played user-top-read user-read-playback-position user-read-playback-state user-modify-playback-state user-read-currently-playing app-remote-control streaming playlist-modify-public playlist-modify-private playlist-read-private playlist-read-collaborative user-follow-modify user-follow-read user-library-modify user-library-read user-read-email user-read-private";
+
+async function refreshAccessToken(token) {
+  try {
+    const url = "https://accounts.spotify.com/api/token";
+    const concatenatedString = `${process.env.SPOTIFY_CLIENT_ID}:${process.env.SPOTIFY_CLIENT_SECRET}`;
+
+    const response = await fetch(url, {
+      headers: {
+        Authorization: `Basic ${btoa(concatenatedString)}`,
+        "Content-Type": "application/x-www-form-urlencoded",
+      },
+      form: {
+        grant_type: "refresh_token",
+        refresh_token: token?.refreshToken,
+      },
+      method: "POST",
+    });
+
+    const refreshedTokens = await response.json();
+
+    if (!response.ok) {
+      throw refreshedTokens;
+    }
+
+    return {
+      ...token,
+      accessToken: refreshedTokens.access_token,
+      accessTokenExpires: Date.now() + refreshedTokens.expires_at * 1000,
+      refreshToken: refreshedTokens.refresh_token ?? token.refreshToken, // Fall back to old refresh token
+    };
+  } catch (error) {
+    console.log(error);
+
+    return {
+      ...token,
+      error: "RefreshAccessTokenError",
+    };
+  }
+}
 
 export default NextAuth({
   providers: [
@@ -14,16 +55,27 @@ export default NextAuth({
     }),
   ],
   callbacks: {
-    async jwt({ token, account }) {
+    async jwt({ token, account, user }) {
       if (account) {
         token.id = account?.id;
         token.accessToken = account?.access_token;
+        (token.accessTokenExpires = Date.now() + account?.expires_at * 1000),
+          (token.refreshToken = account?.refresh_token);
       }
-      return token;
+
+      // Return previous token if the access token has not expired yet
+      if (Date.now() < token.accessTokenExpires) {
+        return token;
+      }
+
+      // Access token has expired, try to update it
+      return refreshAccessToken(token);
     },
 
     async session({ session, token }) {
       session.accessToken = token?.accessToken;
+      session.refreshToken = token?.refreshToken;
+      session.error = token?.error;
       return session;
     },
   },
