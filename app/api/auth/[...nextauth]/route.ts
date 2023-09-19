9357bf02af7b5b6944fd29ab4e3215a2e7c8a144
0xTxbi <techiejossy@gmail.com>
2023-09-19 22:09:34 +0100

    feat: update spotify provider callbacks

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,13 +1,36 @@
 import NextAuth from "next-auth";
 import SpotifyProvider from "next-auth/providers/spotify";
+import { JWT } from "next-auth/jwt";
+import { User, Account, Session as NextAuthSession } from "next-auth";
+
+interface IToken extends JWT {
+	accessToken?: string;
+}
+
+interface Session extends NextAuthSession {
+	accessToken?: string;
+}
 
 const handler = NextAuth({
 	providers: [
 		SpotifyProvider({
-			clientId: process.env.SPOTIFY_CLIENT_ID || "",
-			clientSecret: process.env.SPOTIFY_CLIENT_SECRET || "",
+			clientId: process.env.SPOTIFY_CLIENT_ID as string,
+			clientSecret: process.env
+				.SPOTIFY_CLIENT_SECRET as string,
 		}),
 	],
+	callbacks: {
+		async jwt(token: IToken, user: any, account: any) {
+			if (account) {
+				token.accessToken = account.accessToken;
+			}
+			return token;
+		},
+		async session(session: Session, token: IToken, user: User) {
+			session.accessToken = token.accessToken;
+			return session;
+		},
+	},
 });
 
 export { handler as GET, handler as POST };
