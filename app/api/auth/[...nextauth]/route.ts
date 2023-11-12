f5dadb66da9ea82a6b3a0058000b27e27cfe9d11
0xTxbi <techiejossy@gmail.com>
2023-11-12 01:46:36 +0100

    feat: add necessary scopes

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,13 +1,17 @@
-import NextAuth from "next-auth";
+import NextAuth, { NextAuthOptions } from "next-auth";
 import SpotifyProvider from "next-auth/providers/spotify";
-import { JWT } from "next-auth/jwt";
 
-const handler = NextAuth({
+export const authOptions = {
 	providers: [
 		SpotifyProvider({
 			clientId: process.env.SPOTIFY_CLIENT_ID as string,
 			clientSecret: process.env
 				.SPOTIFY_CLIENT_SECRET as string,
+			authorization: {
+				params: {
+					scope: "user-read-email user-top-read user-read-private user-library-read user-read-currently-playing",
+				},
+			},
 		}),
 	],
 	callbacks: {
@@ -28,6 +32,8 @@ const handler = NextAuth({
 			return session;
 		},
 	},
-});
+} satisfies NextAuthOptions;
+
+const handler = NextAuth(authOptions);
 
 export { handler as GET, handler as POST };
