28dabc12f0e55a9bfcb479738ac99c7ea2f470fe
0xTxbi <techiejossy@gmail.com>
2023-11-13 13:58:22 +0100

    feat: retrieve track audio features hook

---
+++ lib/hooks/useTrackFeatures.ts
@@ -0,0 +1,33 @@
+import useAuthenticatedSWR from "./useAuthSWR";
+
+interface TrackFeatures {
+	id: string;
+	name: string;
+}
+
+interface TrackFeaturesHookResult {
+	error: Error | null;
+	loading: boolean;
+	trackFeaturesInfo: TrackFeatures | null;
+}
+
+function useTrackFeatures({ id }: { id: string }): TrackFeaturesHookResult {
+	const { data, error, isLoading } = useAuthenticatedSWR<TrackFeatures>(
+		`https://api.spotify.com/v1/audio-features/${id}`
+	);
+
+	console.log(data, error, isLoading);
+
+	// loading and error states
+	if (isLoading) {
+		return { error: null, loading: true, trackFeaturesInfo: null };
+	}
+
+	if (error) {
+		return { error, loading: false, trackFeaturesInfo: null };
+	}
+
+	return { error: null, loading: false, trackFeaturesInfo: data };
+}
+
+export default useTrackFeatures;
